041281192c8f1e5cda39ea22e89c3720d12cc892
Ana Marija Dimitrova <ad81532@student.uni-lj.si>
2025-07-03 10:07:56 +0200

    Final.py

--- Final.py
+++ Final.py
@@ -1,10 +1,9 @@
-# Fix SQLite version issue for ChromaDB on Streamlit Cloud
 try:
-    _import_('pysqlite3')
+    __import__('pysqlite3')
     import sys
     sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 except ImportError:
-    pass
+    pass
 
 import streamlit as st
 import chromadb
